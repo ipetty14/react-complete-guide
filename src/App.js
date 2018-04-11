353d2312b03d09ec81a6c10611623a0794c80024
ipetty14 <ipetty@icloud.com>
2018-04-11 12:45:07 -0600

    Started creating custom component Person

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    return (
+     return (
       <div className="App">
-      <h1>Hi, I am a react app</h1>
+       <h1>Hi, I am a react app</h1>
+       <Person />
       </div>
-    );
+     );
+
+    // Above code in jsx gets compiled to this in React
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'), 'Hi, I\'m a react app!!!');
   }
 }
 
