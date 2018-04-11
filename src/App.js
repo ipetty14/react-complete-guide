b383c291d49bd6390d1e9d8cf7967d8b6e0fafff
ipetty14 <ipetty@icloud.com>
2018-04-10 23:51:39 -0600

    Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hi, I am a react app</h1>
+      <h1>Hi, I am a react app</h1>
       </div>
     );
   }
