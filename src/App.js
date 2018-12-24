d567fade86e66e204034c08337c85e63c8588fae
ipetty14 <ipetty@masonite.com>
2018-12-24 12:33:28 -0700

    commit changes to master

--- src/App.js
+++ src/App.js
@@ -1,98 +1,112 @@
 import React, { Component } from 'react';
-import './App.css';
 import Person from './Person/Person';
-import Radium from 'radium';
+import './App.css';
 
 class App extends Component {
   // State is a way to create dynamic components in the App
   state = {
     persons: [
-      { name: 'Ian', age: 22 },
-      { name: 'Sofia', age: 20 },
-      { name: 'Patrick', age: 17 }
-    ]
-  }
+      { id: 'ijp', name: 'Ian', age: 22 },
+      { id: 'spp', name: 'Sofia', age: 20 },
+      { id: 'pgp', name: 'Patrick', age: 17 }
+    ],
+    showPersons: false
+  };
 
-  // ES6 Syntax needed to use 'this' keyword
-  switchNameHandler = ( newName ) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS => this.state.persons[2].name='Meghan';
-    this.setState({
-      persons: [
-        { name: 'Ian', age: 22 },
-        { name: 'Sofia', age: 20 },
-        { name: newName, age: 17 }
-      ]
-    })
-  }
+  nameChangedHandler = (event, id) => {
+    // Done completely without mutating the actual state. Everything is a copy
+    const personIndex = this.state.persons.findIndex(p => p.id === id);
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+    const persons = [...this.state.persons];
 
-  nameChangedHandler = ( event ) => {
-    this.setState({
-      persons: [
-        { name: 'Ian', age: 22 },
-        { name: event.target.value, age: 20 },
-        { name: 'Patrick', age: 17 }
-      ]
-    })
-  }
+    person.name = event.target.value;
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  };
+
+  deletePersonHandler = index => {
+    // Slice method used to create safe copy of data in array
+    // so the actual state is not being spliced.
+    // const persons = this.state.persons.slice();
+
+    // ES6 spread operator creates a copy like the slice method below
+    const persons = [...this.state.persons];
+    persons.splice(index, 1);
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
 
   render() {
     const styles = {
-      buttonClass: {
-        backgroundColor: '#79bbff',
-        borderRadius: '6px',
-        border: '1px solid #84bbf3',
-        display: 'inline - block',
-        cursor: 'pointer',
-        color: '#ffffff',
-        fontFamily: 'Arial',
-        fontSize: '15px',
-        fontWeight: 'bold',
-        padding: '6px 24px',
-        textDecoration: 'none',
-        textShadow: '0px 1px 0px #528ecc',
-
-        ':hover': {
-          backgroundColor: '#378de5',
-        },
-
-        ':active': {
-          position: 'relative',
-          top: '1px',
-        },
-      },
+      backgroundColor: '#1976d2',
+      borderRadius: '6px',
+      border: '1px solid #84bbf3',
+      display: 'inline - block',
+      cursor: 'pointer',
+      color: '#ffffff',
+      fontFamily: 'Arial',
+      fontSize: '15px',
+      fontWeight: 'bold',
+      padding: '6px 24px',
+      textDecoration: 'none',
+      textShadow: '0px 1px 0px #528ecc',
+      outline: '0'
     };
 
-     return (
+    let persons = null;
+
+    if (this.state.showPersons) {
+      styles.backgroundColor = '#d32f2f';
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                click={() => this.deletePersonHandler(index)}
+                name={person.name}
+                age={person.age}
+                key={person.id}
+                changed={event => this.nameChangedHandler(event, person.id)}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+
+    let classes = [];
+
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+    }
+
+    return (
       <div className="App">
         <h1>Hi, I am a react app</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
         {/* Don't call handler with () because that calls it immediately upon loading and not on the acutal click */}
         {/* Below syntax with parameter is ok but bind syntax is better for rendering purposes.
             bind function is more efficient than arrow function syntax. */}
-         <button
-          style={styles.buttonClass}
-          onClick={() => this.switchNameHandler('Patty')}>Switch Name</button>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          changed={this.nameChangedHandler}> My Hobbies: Makeup, Video Production</Person>
-        {/* 'click' becomes part of props and allows for a change in
-            the state from outside functions that are not directly part
-            of the App container here. This helps reduce the number of
-            components in the program while still giving flexibility.  */}
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}>My Hobbies: Basketball, Coding</Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}
-          click={this.switchNameHandler.bind(this, 'Pat')}>My Hobbies: JROTC, Shooting</Person>
+        <button style={styles} onClick={this.togglePersonsHandler}>
+          Toggle Persons
+        </button>
+        {persons}
       </div>
-     );
+    );
 
     // Above code in jsx gets compiled to this in React
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'), 'Hi, I\'m a react app!!!');
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,19 @@
-import React from 'react'; 
+import React from 'react';
 import './Person.css';
 
-const person = ( props ) => {
-    return (
-        <div className="Person">
-            <h3 onClick={props.click}><u>I am {props.name} and I am {props.age} years old!</u></h3>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed} placeholder={props.name} />
-        </div> 
-    )
+const person = props => {
+  
+  return (
+    <div style={styles}>
+      <h3 onClick={props.click}>
+        <u>
+          I am {props.name} and I am {props.age} years old!
+        </u>
+      </h3>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} placeholder={props.name} />
+    </div>
+  );
 };
 
 export default person;
